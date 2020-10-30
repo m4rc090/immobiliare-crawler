60d71c342d1f443ba997e77448ebcb30ca528b1d
Marco Coluzza <m.coluzza@ilivetech.it>
2020-10-30 11:28:01 +0100

    fix

--- immobiliare_crawler/apps/main.py
+++ immobiliare_crawler/apps/main.py
@@ -2,8 +2,9 @@ from immobiliare_crawler.business.daemons import JobCralwer
 from immobiliare_crawler.business.report import ReportGenerator
 from immobiliare_crawler.dao.daos import ZoneRomaDao, ImmobiliareCaseDao
 
-if __name__ == '__main__':
 
+def app(run_crawler: bool):
+    params = {"prezzo_minimo": 160000, "prezzo_massimo": 280000, "superficie_minima": 40, "superficie_massima": 100}
     zone = ["Eur, Torrino, Tintoretto", "Garbatella, Navigatori, Ostiense", "Marconi, San Paolo"]
     zone_dao = ZoneRomaDao()
 
@@ -11,18 +12,13 @@ if __name__ == '__main__':
     print(id_zone)
 
     dao_case = ImmobiliareCaseDao(collection="case_collection_1")
-    job = JobCralwer(prezzo_minimo=160000,
-                     prezzo_massimo=280000,
-                     superficie_minima=40,
-                     superficie_massima=100,
-                     zone=id_zone, dao_case=dao_case)
+    if run_crawler:
+        job = JobCralwer(**params, zone=id_zone, dao_case=dao_case)
+        job.run_crawler()
 
-    # job.run_crawler()
-
-    report_generator = ReportGenerator(prezzo_minimo=160000,
-                                       prezzo_massimo=280000,
-                                       superficie_minima=40,
-                                       superficie_massima=100,
-                                       nomi_zone=zone,
-                                       dao_case=dao_case)
+    report_generator = ReportGenerator(**params, nomi_zone=zone, dao_case=dao_case)
     report_generator.generate("/home/marco/Documents/MarcoDocs/my-projects/immobiliare-crawler")
+
+
+if __name__ == '__main__':
+    app(True)
